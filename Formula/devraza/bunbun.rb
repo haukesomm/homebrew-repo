a9152600f229e31de58cbbc0a033b55debc10249
Hauke Sommerfeld <development@haukesomm.de>
2024-03-31 17:18:00 +0200

    Fix bunbun test

--- Formula/devraza/bunbun.rb
+++ Formula/devraza/bunbun.rb
@@ -1,5 +1,5 @@
 class Bunbun < Formula
-  desc "A simple and adorable sysinfo utility written in Rust"
+  desc "Simple and adorable sysinfo utility written in Rust"
   homepage "https://github.com/devraza/bunbun"
   url "https://github.com/devraza/bunbun/archive/refs/tags/v1.3.0.tar.gz"
   sha256 "33a5a8c996322d26594b2a3195d67d07d7ea48e2dd35ac735d337ad83205be28"
