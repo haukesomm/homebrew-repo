df5f615b84ae5651f76965bda588a73e919931b7
Hauke Sommerfeld <development@haukesomm.de>
2024-04-11 19:09:13 +0200

    Update apple-photos-export formula to version 0.1.0

--- Formula/haukesomm/apple-photos-export.rb
+++ Formula/haukesomm/apple-photos-export.rb
@@ -1,8 +1,8 @@
 class ApplePhotosExport < Formula
   desc "Command-line tool to export photos from the macOS Photos library"
   homepage "https://github.com/haukesomm/apple-photos-export"
-  url "https://github.com/haukesomm/apple-photos-export/archive/refs/tags/release-0.0.1.tar.gz"
-  sha256 "c484246192cc1ff81b8b6fe92902e84e140454f6dd19213c585aafe465c4cab4"
+  url "https://github.com/haukesomm/apple-photos-export/archive/refs/tags/release-0.1.0.tar.gz"
+  sha256 "5fb3e8dda13faa4b80b7dd9ab2500b23677a4a24eb1b27290221c6407bad4dd5"
   license "MIT"
 
   depends_on "rust" => :build
@@ -13,6 +13,6 @@ class ApplePhotosExport < Formula
 
   test do
     output = shell_output("#{bin}/apple-photos-export --version")
-    assert_match "apple-photos-export 0.0.1", output
+    assert_match "apple-photos-export 0.1.0", output
   end
 end
