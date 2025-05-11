2632b9a635d2ad68ce1e33b3cfa7d378d3ffcf8d
Hauke Sommerfeld <development@haukesomm.de>
2025-05-11 22:10:03 +0200

    Update bunbun to versino 1.5.0

--- Formula/devraza/bunbun.rb
+++ Formula/devraza/bunbun.rb
@@ -1,8 +1,8 @@
 class Bunbun < Formula
   desc "Simple and adorable sysinfo utility written in Rust"
   homepage "https://github.com/devraza/bunbun"
-  url "https://github.com/devraza/bunbun/archive/refs/tags/v1.3.0.tar.gz"
-  sha256 "33a5a8c996322d26594b2a3195d67d07d7ea48e2dd35ac735d337ad83205be28"
+  url "https://github.com/devraza/bunbun/archive/refs/tags/v1.5.0.tar.gz"
+  sha256 "dff37adede7fb821339eddb1d455f25dcb0d1bfa51527a446c47d88f253a878d"
   license "MIT"
 
   depends_on "rust" => :build
@@ -13,6 +13,6 @@ class Bunbun < Formula
 
   test do
     output = shell_output("#{bin}/bunbun --version")
-    assert_match "bunbun 1.3.0", output
+    assert_match "bunbun 1.5.0", output
   end
 end
--- README.md
+++ README.md
@@ -13,4 +13,4 @@ natively support homebrew.
 
 | Project | Author                                | Version | Repo                              |
 | ------- | ------------------------------------- | ------- | --------------------------------- |
-| bunbun  | [devraza](https://github.com/devraza) | `1.3.0` | https://github.com/devraza/bunbun |
+| bunbun  | [devraza](https://github.com/devraza) | `1.5.0` | https://github.com/devraza/bunbun |
